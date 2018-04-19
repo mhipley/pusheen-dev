dbfc31c5b25712aa06a4cc3e8ce5237527124e2f
Martha Hipley <info@marthahipley.com>
2018-04-18 23:32:45 -0500

    contact page

--- wp-content/themes/pusheen-custom/page-about.php
+++ wp-content/themes/pusheen-custom/page-about.php
@@ -1,45 +1,12 @@
 <?php /* Template Name: About Page */ get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
-		<section>
-
-			<h1><?php the_title(); ?></h1>
-
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
-
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
-
-			</article>
-			<!-- /article -->
-
-		<?php endwhile; ?>
-
-		<?php else: ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-			<?php echo do_shortcode('[contact-form-7 id="33" title="Contact form 1"]'); ?>
-
-		<?php endif; ?>
-
-		</section>
-		<!-- /section -->
+		<div class="row">
+			<div class="col-sm-12">
+				<h1>Contact Us</h1>
+			</div>
+				<?php echo do_shortcode('[contact-form-7 id="33" title="Contact form 1"]'); ?>
+		</div>
 	</main>
 
 
